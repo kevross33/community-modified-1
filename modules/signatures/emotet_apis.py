b6e1b1fecd97fe527af2575a723e0e881f6fad77
kevross33 <kevross33@gmail.com>
2017-05-04 08:22:20 +0100

    Update Emotet Sig

--- modules/signatures/emotet_apis.py
+++ modules/signatures/emotet_apis.py
@@ -15,12 +15,17 @@
 
 from lib.cuckoo.common.abstracts import Signature
 
+try:
+    import re2 as re
+except ImportError:
+    import re
+
 class Emotet_APIs(Signature):
     name = "emotet_behavior"
-    description = "Exhibits behavior characteristic of Emotet/Geodo malware"
+    description = "Exhibits behavior characteristic of Emotet/Geodo Malware"
     weight = 3
     severity = 3
-    categories = ["infostealer"]
+    categories = ["infostealer", "banker"]
     families = ["Emotet"]
     authors = ["Kevin Ross"]
     minimum = "1.3"
@@ -30,8 +35,12 @@ class Emotet_APIs(Signature):
         Signature.__init__(self, *args, **kwargs)
         self.compname = []
         self.processlist = []
+        self.c2 = []
+        self.isemotet = False
+        self.lasthandle = str()
+        self.ip = str()
 
-    filter_apinames = set(["GetComputerNameW", "Process32NextW", "CryptEncrypt"])
+    filter_apinames = set(["GetComputerNameW", "Process32NextW", "CryptEncrypt", "InternetConnectW", "HttpOpenRequestW", "HttpSendRequestW"])
 
     def on_call(self, call, process):
         if call["api"] == "GetComputerNameW":
@@ -49,11 +58,36 @@ class Emotet_APIs(Signature):
         elif call["api"] == "CryptEncrypt":
             if call["status"]:
                 buf = self.get_argument(call, "Buffer")
-                for compname in self.compname:
-                    if compname in buf:
-                        count = 0
-                        for proc in self.processlist:
-                            if proc in buf:
-                                count += 1
-                        if count > 5:
-                            return True
+                if "[System Process]" in buf:
+                    for compname in self.compname:
+                        if compname in buf:
+                            count = 0
+                            for proc in self.processlist:
+                                if proc in buf:
+                                    count += 1
+                            if count > 5:
+                                self.emotet = True
+
+        elif call["api"] == "InternetConnectW" and self.emotet:
+            ip = self.get_argument(call, "ServerName")
+            port = self.get_argument(call, "ServerPort")
+            if ip not in self.c2:
+                self.ip = ip
+                self.lasthandle = call["return"]
+
+        elif call["api"] == "HttpOpenRequestW" and self.emotet:
+            handle = self.get_argument(call, "InternetHandle")
+            if handle == self.lasthandle:
+                self.lasthandle = call["return"]
+
+        elif call["api"] == "HttpSendRequestW" and self.emotet:
+            handle = self.get_argument(call, "RequestHandle")
+            if handle == self.lasthandle:
+                headers = self.get_argument(call, "Headers")
+                if re.match(r"^Cookie\:\ [A-Za-z0-9]{3,4}=(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$", headers):
+                    self.data.append({"C2": self.ip})
+                    self.c2.append(self.ip)
+
+    def on_complete(self):
+        if self.emotet:
+            return True
